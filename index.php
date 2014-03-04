8e7c4cdf55bac618ce7c3e40fdcece721dd4a2af
chikaboo1 <chikaboo1@gmail.com>
2014-03-04 22:32:10 +0100

    not much
    added the switch case and changed a few things here and there

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+$out="";
+
+if (isset($_GET['page'])){
+
+        switch($_GET['page']){
+
+                case 'aboutus':
+                    include("aboutus.php");
+                    break;
+
+                case 'winners':
+                    include("winners.php");
+                    break;
+
+                case 'prizes':
+                    include("prizes.php");
+                    break;
+
+                case 'activate':
+                    include("activation.php");
+                    break;
+
+                    case 'campaign':
+                    include("campaign.php");
+                    break;
+
+                  default:
+                    include("campaign.php");
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <html>
@@ -19,10 +53,10 @@
             <div id="main-wrapper">
                 <!-- wrapper for all the content within this side of the site -->
                 <section id='main-content-wrapper'>
-                    <p>Hello! We're<a href="#"><img src="img/randoms.png" alt="randoms"/></a> and we are <a href=""><img src="img/running.png" alt=""></a> a <a href="#">campaign</a> where<span> 1/50 </span>of<a href="#"><img src="img/candy-bag.gif" alt="randoms"/></a> has <a><img src="img/prize.gif" alt=""></a> in it. We're giving away <a href="#"><img src="img/rostick.png"></a>and much more. If you're a <a href=""><img src="img/winner.jpg" alt=""></img></a> </p>
-                    <form action="activation.php" method="get" enctype="multipart/form-data" style="display:inline;z-index:100;">
+                    <p>Hello! We're<a href="index.php?page=aboutus" class="link"><img src="img/randoms.png" alt="randoms"/></a> and we are <a href="index.php?page=campaign" class="link"><img src="img/running.png" alt=""></a> a <a href="index.php?page=campaign" class="link">campaign</a> where<span> 1/50 </span>of<a href="index.php?page=aboutus" class="link"><img src="img/candy-bag.gif" alt="randoms"/></a> has <a href="index.php?page=prizes" class="link"><img src="img/prize.gif" alt=""></a> in it. We're giving away <a href="index.php?page=prizes" class="link"><img src="img/rostick.png"></a>and much more. If you're a <a href="index.php?page=winners" class="link"><img src="img/winner.jpg" alt=""></img></a> </p>
+                    <form action="#" method="get" enctype="multipart/form-data" style="display:inline;z-index:100;">
                     <input type="text" name="activationcode" placeholder="activate your code..." style="width:150px; height:30px;">
-                    <input type="submit" name="go" value="Go" id="activate" class="button"></form>
+                    <input type="submit" name="page" value="activate" id="activate"></form>
                     <p>and tag us <a>#rowntrees</a> in your
                     <a href=""><i class= 'fa fa-instagram'> </i></a>
                     <a href=""><i class= 'fa fa-facebook-square'> </i></a>
