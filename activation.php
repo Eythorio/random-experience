9aca3e0e07de223249cdbd3b9644d1ed8e955f34
chikaboo1 <chikaboo1@gmail.com>
2014-03-04 22:33:56 +0100

    activation
    so like i mentioned before you cant contact the database unless your in
    it so this php file only works when its on janis is server. so if you
    go to janis0809.keaweb.dk/random-exp and use a valid code eg. abc00
    then this works. there are no images because i didnt upload the images
    folder because that would have used too much of my phone’s data :)  i
    think thats it

--- activation.php
+++ activation.php
@@ -1,25 +1,27 @@
 <?php
 include("dbconnect.php");
 
-if (!$link) {
-    die('Not connected : ' . mysql_error());
+$out.="
+<h1>Congratulations!</h1>
+<p>Choose from the following selection of prizes</p>
+<form action='#' method='get' enctype='multipart/form-data'>";
+
+
+//getting the category of given code
+$code=$_GET['activationcode'];
+$resultcategory = mysql_query("SELECT category FROM re_activation_code where code='$code'");	
+$datacategory=mysql_fetch_assoc($resultcategory);
+$category=$datacategory['category'];
+
+
+$prizes="SELECT * FROM re_prize where category='$category'";
+$resultprizes=mysql_query($prizes);
+
+while($dataprizes=mysql_fetch_assoc($resultprizes)){
+$prizename=$dataprizes['name'];
+$out.="<input type='radio' name='prize' value='$prizename'>$prizename<br>";
 }
 
+$out.="<input type='submit' name='submit' value='activate'></form>"
 
 ?>
-
-<!doctype html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-	<titLe>Activation</title>
-</head>
-<body>
-	<form action="index.php" method="post" enctype="multipart/form-data">
-			<label for="code"> Activation Code: </label>
-		  	<input type="text" name="code"  id="code">
-			<input type="submit" name="submit" value="Activate" class="button">
-	</form>
-	<p><?php echo $out;?></p>
-</body>
-</html>
