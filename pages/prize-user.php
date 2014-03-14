252274ab2ccf41c45b30de5ef3de78a6ee55f7c5
Luke Pommersheim <lpommers@gmail.com>
2014-03-14 09:44:37 +0100

    added form classes and styles

--- pages/prize-user.php
+++ pages/prize-user.php
@@ -13,9 +13,9 @@
 
 	echo "<p>Almost done. You chose  $prize in $city. We just need to know who you are so we can send you your prize!</p>
 			<form action='user-confirmation.php' method='get' enctype='multipart/form-data'><br>
-			<input type='text' id='fname' name='fname' placeholder='First Name' required/><br>
-			<input type='text' id='lname' name='lname' placeholder='Last Name' required/><br>
-			<input type='text' id='email' name='email' placeholder='Email Address' required/><br>
+			<input type='text' id='fname' class= 'user-input-fields' name='fname' placeholder='First Name' required/><br>
+			<input type='text' id='lname' class= 'user-input-fields' name='lname' placeholder='Last Name' required/><br>
+			<input type='text' id='email' class= 'user-input-fields' name='email' placeholder='Email Address' required/><br>
 
 			<input id='user-button'type='button' class='form-submit-button' value='submit' onClick='SubmitConfirmation();'></form>";
  ?>
